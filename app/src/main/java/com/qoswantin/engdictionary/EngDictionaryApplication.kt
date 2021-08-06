193f3e9e926b6bfa78f4a3a8325feebdb1e9f9d3
Aleksandr Efimov <A.Efimov@stream.ru>
2021-08-06 21:02:25 +0300

    Add common dependencies

---
+++ app/src/main/java/com/qoswantin/engdictionary/EngDictionaryApplication.kt
@@ -0,0 +1,12 @@
+package com.qoswantin.engdictionary
+
+import android.app.Application
+
+class EngDictionaryApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //TODO init dependencies
+    }
+
+}
